730548fd6539a883b02a007c7aa4dac4f6c2c098
helmogey <h.elmogey@gmail.com>
2024-03-05 09:34:58 +0200

    base model

---
+++ models/base_model.py
@@ -0,0 +1,8 @@
+
+import uuid
+import datetime
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
